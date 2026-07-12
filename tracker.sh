db9456908d80e0edeb78834711afc7093a7809af
Shrestha Samanta <shresthasamanta977@gmail.com>
2026-07-12 23:37:01 +0530

    This is the second commit V2

--- tracker.sh
+++ tracker.sh
@@ -3,3 +3,12 @@
 echo "========================================="
 echo "      git Activity Tracker"
 echo "========================================="
+
+repo=$(basename "$(git rev-parse --show-toplevel)")
+
+echo "Repository Name : $repo"
+branch=$(git branch --show-current)
+echo "branch : $branch"
+remote=$(git remote get-url origin)
+echo "Remote URL : $remote"
+echo "-------------------------------------------"
