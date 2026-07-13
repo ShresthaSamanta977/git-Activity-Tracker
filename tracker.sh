f8be04080d084fc24b143f0a97aaaf81a3143402
Shrestha Samanta <shresthasamanta977@gmail.com>
2026-07-13 20:42:15 +0530

    This is fifth commit V5

--- tracker.sh
+++ tracker.sh
@@ -26,3 +26,12 @@ last_date=$(git log -1 --pretty=format:"%cd" --date=short)
 echo "Last Commit Date : $last_date"
 last_time=$(git log -1 --pretty=format:"%cd" --date=format-local:"%I:%M:%S %p")
 echo "Last commit Timing: $last_time"
+echo "------------Repository Helth-----------------"
+tracked_files=$(git ls-files | wc -l)
+echo "Tracked files: $tracked_files"
+repo_size=$(du -sh . | cut -f1)
+echo "Repository size: $repo_size"
+git_size=$(du -sh .git | cut -f1)
+echo "git Repository size: $git_size"
+echo "------------------CHECKING-PART-----------------------------"
+
